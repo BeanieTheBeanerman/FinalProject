15cce3692b16298c786f30cc96e8d60833e0353d
nika chkhikvadze <124697694+BeanieTheBeanerman@users.noreply.github.com>
2023-05-26 17:29:20 +0400

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,3 +28,89 @@ function startSliding() {
 }
 
 startSliding();
+
+const squareButton = document.querySelectorAll(".square-button"),
+  profiles = document.querySelectorAll(".recom-profiles");
+
+squareButton.forEach((el, i) => {
+  el.addEventListener("click", () => {
+    changeSquare(i);
+  });
+});
+
+function changeSquare(nextIndex) {
+  activeIndex = nextIndex;
+  renderButtons();
+}
+
+function renderButtons() {
+  profiles.forEach((el, i) => {
+    if (activeIndex === i) {
+      el.classList.add("active");
+    } else {
+      el.classList.remove("active");
+    }
+  });
+
+  squareButton.forEach((el, i) => {
+    if (activeIndex === i) {
+      el.classList.add("active");
+    } else {
+      el.classList.remove("active");
+    }
+  });
+}
+
+const modalOpenBtn = document.querySelector(".button"),
+  modal = document.querySelector(".modal"),
+  modalCloseBtn = document.querySelector(".close-modal");
+
+// function openModal() {
+//   modal.classList.add("pop-up");
+// }
+
+// function closeModal() {
+//   modal.classList.remove("pop-up");
+// }
+
+function dynamicOpenModal(selector) {
+  const modal = document.querySelector(selector);
+  if (modal) {
+    modal.classList.add("pop-up");
+    const modalCloseBtn = modal.querySelector(".close-modal");
+    modalCloseBtn.addEventListener("click", () => {
+      dynamicCloseModal(selector);
+    });
+  }
+}
+
+function dynamicCloseModal(selector) {
+  const modal = document.querySelector(selector);
+  if (modal) {
+    modal.classList.remove("pop-up");
+  }
+}
+
+
+const userUsername = document.getElementById("username"),
+  userEmail = document.getElementById("useremail"),
+  userWebsite = document.getElementById("userwebsite"),
+  userText = document.getElementById("userText"),
+  form = document.getElementById("form");
+
+async function registerUser(userData) {
+  try {
+    const send = await fetch("http://borjomi.loremipsum.ge/api/send-message", {
+      method: "POST",
+      body: JSON.stringify(userData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const dataFrom = await res.JSON();
+    console.log(dataFrom);
+    // await Response.JSON();
+  } catch (e) {
+    console.log("Eroor - ", e);
+  }
+}
